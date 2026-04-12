a6d417dda07037d5c89c25574df142f08da48248
Kyle Ng <kgng@usc.edu>
2026-04-12 08:38:18 +0000

    add jaxtyping requirement and test for finetuning

--- tests/test_decoder_model.py
+++ tests/test_decoder_model.py
@@ -18,6 +18,22 @@ suffix_ids = torch.randint(
 
 pretrain_loss = my_decoder_model.forward_train(soft_token_acts, suffix_ids)
 
+print("#"*20)
 print("Soft Token Activations shape", soft_token_acts.shape)
 print("Suffix IDs shape", suffix_ids.shape)
+
+print("#"*20)
 print("pretraining loss on sample soft tokens acts and suffix ids", pretrain_loss)
+
+context_ids = torch.randint(
+     low=0, high=config.n_vocab, size=[batch, config.n_middle], device=config.device
+)
+fine_tune_loss = my_decoder_model.forward_train(
+    soft_token_acts=soft_token_acts, 
+    context_ids=context_ids,
+    target_ids=suffix_ids,
+    soft_token_mask=torch.ones(batch, config.n_prefix, device=config.device)
+)
+
+print("#"*20)
+print("finetune loss on sample soft tokens acts and suffix ids", fine_tune_loss)
